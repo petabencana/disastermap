dd54905b640287107230377dd9b7eaaa0b96690a
Ssravani272 <sravanisruthi272@gmail.com>
2024-09-09 12:32:23 +0530

    fix the need onclick icon path

--- src/components/disaster-map/map-layers.js
+++ src/components/disaster-map/map-layers.js
@@ -876,6 +876,26 @@ export class MapLayers {
         return this.addReportsClustered(endPoint, cityName, map, togglePane);
     }
 
+    addArchiveReports(dates, map, togglePane) {
+        let self = this;
+        if (self.reports) {
+            // map.removeLayer(self.reports);
+            self.reports = null;
+        }
+        const cityName = "Daerah Khusus Ibukota Jakarta";
+        if (Array.isArray(dates) && dates.length > 0) {
+            const startDate = dates[0].toISOString().split("T")[0];
+            const endDate = dates[1].toISOString().split("T")[0];
+            // https://api.petabencana.id/reports/archive?start=2023-10-30T00%3A00%3A00%2B0700&end=2023-10-30T23%3A00%3A00%2B0700&geoformat=geojson
+            let endPoint = `reports/archive?start=${startDate}T00%3A00%3A00%2B0700&end=${endDate}T23%3A00%3A00%2B0700&training=false`;
+            // add layer to map
+            // return self.appendData('reports/?admin=' + cityRegion + '&timeperiod=' + self.config.report_timeperiod, self.reports, map);
+            return this.addReportsClustered(endPoint, cityName, map, togglePane);
+        }
+        let endPoint = `reports/?admin=ID-JK&training=${self.config.environment === "training"}`;
+        return this.addReportsClustered(endPoint, cityName, map, togglePane);
+    }
+
     addReportsClustered(endPoint, cityName, map, togglePane) {
         let self = this;
         return new Promise((resolve, reject) => {
